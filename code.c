76f167dec0cd3f44c346d503337ea224a7ccca12
biprendra kumar <181478429+viprendrajiji@users.noreply.github.com>
2025-04-09 23:28:49 +0530

    code.c

--- code.c
+++ code.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+#include "basic_codes.h"
+
+int main() {
+    int a = 10, b = 20;
+    printf("Sum: %d\n", sum(a, b));
+
+    int n = 5;
+    printf("Factorial of %d: %d\n", n, factorial(n));
+
+    int check = 8;
+    if (is_even(check)) {
+        printf("%d is even.\n", check);
+    } else {
+        printf("%d is odd.\n", check);
+    }
+
+    return 0;
+}
